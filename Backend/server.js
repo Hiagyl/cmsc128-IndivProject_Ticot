b8cc7c5f2a202ac4aa003f5758d1e651cd7d8173
Hiagyl <joshuaticot@gmail.com>
2025-09-24 10:40:43 +0800

    Fix bugs in server

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 mongoose
     .connect(
-        process.env.MONGODB_URI || "mongodb://localhost:27017"
+        process.env.MONGODB_URI || "mongodb://localhost:27017/tasks"
     )
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("MongoDB connection error:", err));
