f2dbc32ae204a70c2f9e12e9979152d1fa0cc908
Hiagyl <joshuaticot@gmail.com>
2025-11-17 10:35:44 +0800

    Add userId in task schema

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -2,13 +2,23 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true   // ensures every task belongs to a user
+    },
+
     title: { type: String, required: true },
     description: { type: String },
+
     priority: { type: String, enum: ["high", "mid", "low"], default: "mid" },
-    dueDateString: { type: String }, // for string input requirement
-    dueDate: { type: Date },         // for expanded features
+
+    dueDateString: { type: String },
+    dueDate: { type: Date },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
+
     completed: { type: Boolean, default: false },
     deleted: { type: Boolean, default: false },
     deletedAt: { type: Date }
